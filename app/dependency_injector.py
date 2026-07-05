052abaaead80e6f2d032530c9ea79ef22ed9e58a
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-07-05 20:32:45 -0300

    feat: ad dependency injection the database for client reposytory

--- app/dependency_injector.py
+++ app/dependency_injector.py
@@ -3,11 +3,15 @@ from fastapi import Depends
 
 from app.data_base.local import LocalDataBase
 from app.data_base.client_repository import ClientRepository
+from app.data_base.user_repository import UserRepository
 
 data_base = LocalDataBase()
 
 def get_data_base() -> LocalDataBase:
     return data_base
 
-def get_client_repository (data_base: Annotated[LocalDataBase, Depends(get_data_base)]) -> ClientRepository:
+def get_client_repository(data_base: Annotated[LocalDataBase, Depends(get_data_base)]) -> ClientRepository:
     return ClientRepository(data_base)
+
+def get_user_repository(data_base: Annotated[LocalDataBase, Depends(get_data_base)]) -> UserRepository:
+    return UserRepository(data_base)
